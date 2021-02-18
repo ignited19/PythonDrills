b3eeba22bd55344acc91ff5dc0fdbcf76cc11776
ignited19 <38870344+ignited19@users.noreply.github.com>
2021-02-18 15:01:46 -0800

    Update Python_Drills.py

--- Python_Drills.py
+++ Python_Drills.py
@@ -1,8 +1,35 @@
 import StringOperations as SO
+import Node as Node
 
+
+print("Let's rock")
 #=========================================================
-# Python Drills
+# Python Drills - Strings
 #=========================================================
 
 #SO.StringOperations.UniqueCharCheck()
 SO.StringOperations.CheckStringDiff()
+
+#=========================================================
+# Python Drills - Strings
+#=========================================================
+
+#Nodes to create a tree
+root = Node.Node("root")
+a = Node.Node("a")
+b = Node.Node("b")
+c = Node.Node("c")
+d = Node.Node("d")
+e = Node.Node("e")
+f = Node.Node("f")
+
+#Create edges towards the nodes
+root.children = [a, b, c, d]
+a.children.append(e)
+a.children.append(f)
+
+
+#Check to verify what is in the graph
+#root.display_tree(root.children)
+
+root.bfs(root)
